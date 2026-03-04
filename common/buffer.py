af718733eb070cab6b8e420828f4229fba1062a5
Dar-rius <mohamedtine17@gmail.com>
2026-03-04 18:19:12 +0000

    first push

---
+++ common/buffer.py
@@ -0,0 +1,55 @@
+import torch
+from torch import Tensor
+import numpy as np
+
+class Buffer:
+    """
+    0 -> Micro State 
+    1 -> Macro State
+    2 -> Action
+    3 -> Old Log Probs 
+    4 -> Returns
+    5 -> Advantage
+    6 -> Reward
+    7 -> Value
+    8 -> Dones
+    9 -> Target Regime
+    """
+    def __init__(self, obs_space:int, buffer_space:int, device:str="cpu"):
+        self.slice: int = 0
+        self.buffer_space = buffer_space
+        self.device = device
+        self.obs = np.zeros(self.buffer_space, 1)
+        #Casting type of actions
+        self.actions = np.zeros(self.buffer_space)
+        self.old_log_probs = np.zeros(self.buffer_space)
+        self.returns = torch.zeros(self.buffer_space)
+        self.adv = torch.zeros(self.buffer_space)
+        self.rewards = torch.zeros(self.buffer_space)
+        self.values = torch.zeros(self.buffer_space)
+        self.dones = torch.zeros(self.buffer_space)
+
+    def add_var(self, )
+
+    def insert(self, obs:np.array, action:np.array, old_log_prob:np.array,  reward:np.array, value:np.array, dones:np.array, target_regime:np.array):
+        self.obs[self.slice] = obs
+        self.actions[self.slice] = action
+        self.old_log_probs[self.slice] = old_log_prob
+        self.rewards[self.slice] = reward
+        self.values[self.slice] = value
+        self.dones[self.slice] = dones
+        self.slice += 1
+
+    def insert_returns(self, returns:np.array, adv:np.array):
+        self.returns[:] = returns
+        self.adv[:] = adv
+    
+    # sampling data
+    def get_all(self) -> tuple:
+        return (self.obs, self.actions, self.old_log_probs,
+                self.returns, self.adv, self.rewards, 
+                self.values, self.dones)
+
+    # Delete all data
+    def clear(self):
+        self.slice = 0
---
+++ ppo-belief/ppo-belief.py
@@ -0,0 +1,89 @@
+import torch
+from torch import nn
+import Tensor
+from torch.optim import Optimizer 
+
+class PPOTrainer:
+    def __init__(self,
+        lr:float=3e-4, 
+        gamma:float=0.99, 
+        gae_lambda:float=0.95, 
+        clip_eps:float=0.2, 
+        value_coef:float=0.5, 
+        belief_coef:float=0.5, 
+        ent_coef:float=0.01, 
+        device:str="cpu"
+        ):
+        self.lr = lr
+        # Hyperparams PPO
+        self.gamma = gamma
+        self.gae_lambda = gae_lambda
+        self.clip_eps = clip_eps
+        # Total Loss Coefficients
+        self.value_coef = value_coef
+        self.belief_coef = belief_coef
+        self.ent_coef = ent_coef
+        self.mse_loss = nn.MSELoss()
+        self.ce_loss = nn.CrossEntropyLoss()
+        #Device where tensor will be run
+        self.device = device
+
+    def compute_gae(self, rewards:float, values:Tensor, last_value:Tensor, dones:Tensor) -> tuple[Tensor]:
+        gae: float = 0.0
+        mask = 1.0 - dones
+        next_values = torch.cat((values[1:], last_value), 0)
+        total_size = rewards.size(0)
+        advantages = torch.zeros_like(rewards)
+        delta = rewards + self.gamma * next_values * mask - values
+        for step in reversed(range(total_size)):
+            gae = delta[step] + self.gamma * self.gae_lambda * mask[step] * gae
+            advantages[step] =  gae
+        returns = advantages + values
+        return (returns, advantages)
+
+    def _lr_decay(self, lr:float, total_steps:int, step:int, optimizer: Optimizer):
+        frac = 1.0 - (step / total_steps)
+        current_lr = lr * frac
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = current_lr
+
+    # Compute Belief PPO and Update network weights
+    def update(self, model: , optimizer: Optomizer, memory:Buffer, total_steps:int, step:int, batch_size:int=64, epochs:int=10):
+        self._lr_decay(self.lr, total_steps, step)
+        # the target regime (0 -> Stable, 1 -> Volatility, 2 -> Crisis)
+        micro_states, macro_states, actions, old_log_probs, returns, adv, _, _, _, target_regimes = memory.get_all()
+        # Normalize the advantages
+        advantages = (adv - adv.mean()) / (adv.std() + 1e-8)
+        dataset_size = actions.size(0)
+        all_indices = torch.randperm(dataset_size, device=self.device)
+        for _ in range(epochs):
+            for start in (0, dataset_size, batch_size):
+                end = start + batch_size
+                idx = all_indices[start:end]
+                if idx.numel() == 0: continue
+                # Evaluate model again
+                _, new_log_probs, dist_entropy, new_values, belief_logits, belief_entropy = self.model.get_action_and_value(micro_states[idx], macro_states[idx], actions[idx])
+                # Compute Ratio (new Policy / old Policy)
+                logratio = new_log_probs - old_log_probs[idx]
+                ratio = torch.exp(logratio)
+                # Loss PPO
+                idx_adv = advantages[idx].flatten()
+                surr1 = ratio * idx_adv
+                surr2 = torch.clamp(ratio, 1.0 - self.clip_eps, 1.0 + self.clip_eps) * idx_adv
+                policy_loss = -torch.min(surr1, surr2).mean()
+                # Loss Value (Critic) - MSE
+                value_loss = self.mse_loss(new_values.flatten(), returns[idx].flatten())
+                # Loss Belief (Auxiliary) - Cross Entropy
+                belief_loss = self.ce_loss(belief_logits, target_regimes[idx].flatten().long())
+                entropy_loss = dist_entropy.mean()
+                # Total Loss
+                loss = policy_loss + \
+                       (self.value_coef * value_loss) + \
+                       (self.belief_coef * belief_loss) - \
+                       (self.ent_coef * entropy_loss)
+                # Backpropagation
+                self.optimizer.zero_grad(set_to_none=True)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.5)
+                self.optimizer.step()
+        return loss.item(), policy_loss.item(), value_loss.item(), belief_loss.item(), dist_entropy.mean().item()
